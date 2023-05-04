bae9b34e187c5a20bc2f1aed67d728363d564620
paulpenney <paul.penney@gmail.com>
2023-05-04 18:01:21 -0400

    update

--- expressserver/server.js
+++ expressserver/server.js
@@ -1,15 +1,14 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000 // use the dynamic port assigned by Heroku or default to 3000
 const path = require('path');
-
 
 // Render Html File
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'templates/index.html'));
 });
 
-
 app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
   // Code.....
 })
